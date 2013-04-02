b6fd5df94d85bc773433d96694cdc9df3d591913
Ryan Mark <ryan@mrk.cc>
2013-04-02 12:21:01 -0500

    fab will install Amazon's cloudwatch scripts for linux

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import run, env
+from fabric.api import run, sudo, env
 from fabric.context_managers import cd
 
 env.git_repo = "https://github.com/newsapps/awsmonitors.git"
@@ -6,6 +6,10 @@ env.git_repo = "https://github.com/newsapps/awsmonitors.git"
 
 def setup():
     run('git clone %(git_repo)s' % env)
+    sudo('apt-get -y install unzip libwww-perl libcrypt-ssleay-perl')
+    run('wget http://ec2-downloads.s3.amazonaws.com/cloudwatch-samples/CloudWatchMonitoringScripts-v1.1.0.zip')
+    run('unzip CloudWatchMonitoringScripts-v1.1.0.zip')
+    run('rm CloudWatchMonitoringScripts-v1.1.0.zip')
 
 
 def update():
@@ -14,3 +18,9 @@ def update():
         run('git checkout .')
         run('git fetch')
         run('git pull')
+
+
+def install_cron():
+    with cd('awsmonitors'):
+        sudo('cp crontab /etc/cron.d/awsmonitors')
+        sudo('service cron reload')
